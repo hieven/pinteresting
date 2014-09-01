10a1ccffdc4246c8961af49e3d54d655e36d5fdc
Even Chang <kiki44552002@gmail.com>
2014-09-01 22:08:43 +0800

    Implentented Devise for Users

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 ruby '2.1.2'
 
 gem 'bootstrap-sass', '~> 3.2.0.1'
+gem 'devise', '~> 3.3.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.4'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Required for Heroku
+  # Note to set this to your actual host
+  config.action_mailer.default_url_options = { host => 'omn-pinteresting.herokuapp.com' }
+
 end
