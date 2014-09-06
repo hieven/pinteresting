d24897f0b5b49c580b530b5d629f25582d95319f
Even Chang <kiki44552002@gmail.com>
2014-09-06 16:27:38 +0800

    Added name to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :pins
+
+  validates :name, presence: true
 end
